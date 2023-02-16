8da8525977509ac8ce4a8e29a454b523580065a7
Masum Rahman <masumrahman73@gmail.com>
2023-02-17 03:33:04 +0600

    M29 shopping cart done

--- js/common.js
+++ js/common.js
@@ -5,13 +5,25 @@ function getTextElementById(elementId){
 
     return elementValue;
 }
-
+function setTextElementById(elementId, value){
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = value;
+}
 function calculateSubTotal(){
     //calculate total
     const currentPhoneTotal = getTextElementById('phone-total');
     const currentCaseTotal = getTextElementById('case-total');
 
     const currentSubTotal = currentPhoneTotal + currentCaseTotal;
-    const subTotalElement = document.getElementById('sub-total');
-    subTotalElement.innerText = currentSubTotal;
+    setTextElementById('sub-total', currentSubTotal);
+
+    //calculate tax
+    const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+    const taxAmount = parseFloat(taxAmountString);
+    setTextElementById('tax-amount', taxAmount);
+
+    //final 
+    const finalAmount = currentSubTotal + taxAmount;
+    setTextElementById('final-total', finalAmount);
 }
+
